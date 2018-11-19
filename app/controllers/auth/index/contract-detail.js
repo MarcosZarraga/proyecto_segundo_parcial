5a89ae9dfaebe7c8b6935b757767e61dc3c443ad
MarcosZarraga <A01271642@itesm.mx>
2018-11-19 15:45:30 -0600

    19 nov 3.45 pm

--- app/controllers/auth/index/contract-detail.js
+++ app/controllers/auth/index/contract-detail.js
@@ -2,23 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 	actions: {
-			eliminarCont(contract){
-			var resultado=confirm("¿Seguro que quieres borrar?");
-			if (resultado) {
-				contract.destroyRecord().then(()=>{
-					alert("Suscriptor borrado correctamente");
-				});
-			}
-		},
-		editarCont(contract){
-			var resultado=confirm("¿Deseas actualizar esta información?");
-			if (resultado) {
-				contract.save().then(()=>{
-					alert("Registro editado correctamente");
-				});
-			}
-		},
-
 		addStudent(idContract){
 			this.transitionToRoute("auth.index.student",idContract);
 		}
